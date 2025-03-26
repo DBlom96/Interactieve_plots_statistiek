fd51c77c0febb85d05e568ba9f2d47e78698dc74
DBlom96 <dannyblom@hotmail.com>
2025-03-26 11:42:45 +0100

    Update interactive_cls.py
    .

--- interactive_cls.py
+++ interactive_cls.py
@@ -25,19 +25,19 @@ def plot_clt(dist, n_samples, sample_size, **params):
     sample_means = generate_sample_means(dist, n_samples, sample_size, **params)
    
     plt.figure(figsize=(8, 5))
-    sns.histplot(sample_means, bins=30, kde=True, color='blue', label="Sample Means")
+    sns.histplot(sample_means, bins=30, kde=True, color='blue', label="Steekproefgemiddelden")
     plt.axvline(np.mean(sample_means), color='red', linestyle='dashed', label=f'Mean: {np.mean(sample_means):.2f}')
-    plt.suptitle(f"Central Limit Theorem: Means of {dist} Distributed Variables")
-    plt.title(f'Sample Size = {sample_size}, Number of Samples = {n_samples}')
-    plt.xlabel("Sample Mean")
-    plt.ylabel("Frequency")
+    plt.suptitle(f"Centrale limietstelling: gemiddelden van {dist} verdeelde kansvariabelen")
+    plt.title(f'Steekproefgrootte = {sample_size}, aantal steekproeven = {n_samples}')
+    plt.xlabel("Steekproefgemiddelde")
+    plt.ylabel("Frequentie")
     plt.legend()
     st.pyplot(plt)
 
 # Streamlit widgets for user input
-dist_selector = st.selectbox("Distribution:", ["normaal", "uniform", "exponentieel", "binomiaal", "Poisson"])
-sample_size_slider = st.slider("Sample Size:", min_value=1, max_value=500, step=1, value=30)
-n_samples_slider = st.slider("Number of Samples:", min_value=1, max_value=100_000, step=10, value=1000)
+dist_selector = st.selectbox("Kansverdeling:", ["normaal", "uniform", "exponentieel", "binomiaal", "Poisson"])
+sample_size_slider = st.slider("Steekproefgrootte:", min_value=1, max_value=500, step=1, value=30)
+n_samples_slider = st.slider("Aantal steekproeven:", min_value=1, max_value=100_000, step=10, value=1000)
  
 # Specific sliders for each distribution
 if dist_selector == "normaal":
